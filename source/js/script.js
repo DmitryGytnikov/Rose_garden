08f3cb46eda4cf640fdc28b81111f19f160f2ec4
DmitryGytnikov <gytnikovd@mail.ru>
2022-03-03 17:46:30 +0300

    Добавляет верстку main.html (с адаптивом)

--- source/js/script.js
+++ source/js/script.js
@@ -2,5 +2,26 @@
 
 $(function () {
 
+  // // Табы в скрытом меню в header-middle на main.html
+  // $('.tab').hover(function(e) {
+  //   e.preventDefault();
+
+  //   $($(this).closest('.menu-hidden__tabs').find('li').find('a')).removeClass('tab--active');
+  //   $($(this).closest('.menu-hidden__tabs').siblings().find('ul')).removeClass('tabs-content--active');
+
+  //   $(this).addClass('tab--active');
+  //   $($(this).attr('href')).addClass('tabs-content--active');
+  // });
+
+  // // Появляющееся меню в header-middle на main.html
+  // $('.header-middle__burger').hover(function(e) {
+  //   e.preventDefault();
+
+  //   $($(this).closest('.header').find('.menu-hidden')).addClass('menu-hidden--active');
+  // });
+
+
+
+
 
 });
